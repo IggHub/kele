46a32984cee98176181b753156502c5590787bff
Iggy <igoririanto@rocketmail.com>
2016-07-08 17:04:41 -0700

    auth_token fetched, still need to work on error-handling

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,16 +2,26 @@ require 'httparty'
 
 class Kele
   include HTTParty
-
+  #base_uri "https://www.bloc.io/api/v1/sessions"
 
   def initialize(email, password)
-    response = self.class.post(url_endpoint("sessions"), body: {"email": email, "password": password}) #fix url_endpoint('sessions') to append the name.
-    raise InvalidStudentCodeError.new() if response.code == 401
+    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+    if StandardError
+      "error"
+    end
+
+=begin  case response.code
+    when 200
+      puts "All good!"
+    when 404
+      puts "O noes not found!"
+    when 500...600
+      puts "ZOMG ERROR #{response.code}"
+    else
+      puts "Error dodged"
+  end
+=end
     @auth_token = response["auth_token"]
   end
 
-  private
-
-  url_endpoint = 'https://www.bloc.io/api/v1'
-  
 end
