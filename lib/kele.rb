a75a711ffef3a736d03c206ee0a8dca449593ed2
Iggy <igoririanto@rocketmail.com>
2016-07-09 16:40:26 -0700

    added get_mentor_availability json, need  to get it converted to ruby array

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,17 +9,6 @@ class Kele
   def initialize(email, password)
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
     raise "invalid email/pass" if response.code != 200
-=begin  case response.code
-    when 200
-      puts "All good!"
-    when 404
-      puts "O noes not found!"
-    when 500...600
-      puts "ZOMG ERROR #{response.code}"
-    else
-      puts "Error dodged"
-  end
-=end
     @auth_token = response["auth_token"]
   end
 
@@ -28,5 +17,12 @@ class Kele
     response = self.class.get(url, headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    url = 'https://www.bloc.io/api/v1/mentors/'+(mentor_id.to_s)+'/student_availability'
+    response = self.class.get(url, headers:{ "authorization" => @auth_token} )
+    JSON.parse(response.body)
+  end
+
 
 end
